e32d65dee192ad461eff69f32feac63526dc1c46
Haddou Younes <xanodere@gmail.com>
2023-11-01 20:53:54 +0100

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+from wp_algo import *
+import argparse
+
+def WordPiece(text: str,vocab_size=30)->list:
+    """
+    Aggregate all above functions.
+    Using VOCAB_SIZE, the vocabulary's granularity can be defined
+    """
+    count = basic_tokenize(text)
+    v0 = vocab0(count)
+    words_parts = get_words_parts(count)
+    while len(v0) < vocab_size:
+        scores = calculate_score(words_parts, count)
+        # This try block catch an error that happens when VOCAB_SIZE is too high compared to the corpus
+        try:
+            best_pair = max(scores, key=scores.get)
+        except ValueError:
+            print(f"VOCAB_SIZE is to high, maximum VOCAB_SIZE is: {len(v0)}")
+            break
+        lc = best_pair[0]
+        rc = best_pair[1]
+        words_parts = simplify_parts(best_pair, words_parts)
+        tokened_part =  lc + rc[2:] if rc.startswith("##") else lc + rc 
+        v0.append(tokened_part)
+    v0.sort(reverse=True)
+    return v0
+
+def main():
+    parser = argparse.ArgumentParser(description="Tokenize text using WordPiece algorithm")
+    parser.add_argument("text", type=str, help="Text to tokenize")
+    parser.add_argument("--vocab_size", type=int, help="Target vocabulary size (optional)", nargs='?')
+
+    args = parser.parse_args()
+    result = WordPiece(args.text, args.vocab_size) if args.vocab_size else WordPiece(args.text)
+    print(result)
+    return result
+
+if __name__ == "__main__":
+    main()
---
+++ wp_algo.py
@@ -0,0 +1,79 @@
+from collections import Counter
+
+def basic_tokenize(text:str) -> dict:
+    """
+    Pre-tokenization
+    """
+    corpus = []
+
+    tokens = text.split()
+    token_counts = Counter(tokens)
+
+    return token_counts
+
+def vocab0(count: dict) -> list:
+    """
+    Generate a initial vocabulary 
+    """
+    v0 = []
+    for word in count.keys():
+        if word[0] not in v0:
+            v0.append(word[0])
+        for letter in word[1:]:
+            if f"##{letter}" not in v0:
+                v0.append(f"##{letter}")
+    v0.sort()
+    return v0
+
+def get_words_parts(count: dict) -> dict:
+    """
+    Assotiate each words with its parts
+    """
+    words_parts = {}
+    for word in count.keys():
+        parts=[]
+        for a,b in enumerate(word):
+            # words_parts[word] = b if a != 0 else f"##{b}"
+            parts.append(b if a == 0 else f"##{b}")
+        words_parts[word] = parts
+    return words_parts
+
+def calculate_score(parts: dict, count: dict)-> dict:
+    """
+    calculate scores for the paires
+    """
+    letter_freqs = {}
+    pair_freqs = {}
+    for word, freq in count.items():
+        part = parts[word]
+        if len(part) == 1:
+            letter_freqs[part[0]] = letter_freqs.get(part[0], 0) + freq
+            continue
+        for i in range(len(part) - 1):
+            pair = (part[i], part[i + 1])
+            letter_freqs[pair[0]] = letter_freqs.get(pair[0], 0) + freq
+            letter_freqs[pair[1]] = letter_freqs.get(pair[1], 0) + freq
+            pair_freqs[pair] = pair_freqs.get(pair, 0) + freq
+
+
+    scores = {
+        pair: freq / (letter_freqs[pair[0]] * letter_freqs[pair[1]])
+        for pair, freq in pair_freqs.items()
+    }
+    return scores
+
+def simplify_parts(pair: tuple, words_parts: dict)-> dict:
+    """
+    Simplify components list
+    """
+    lc = pair[0]
+    rc = pair[1]
+    for word, part in words_parts.items():
+        if len(part) == 1:
+            continue
+        for i in range(len(part)-1):
+            if part[i] == lc and part[i + 1] == rc:
+                simplified = lc + rc[2:] if rc.startswith("##") else lc + rc 
+                part[i:i+2] = [simplified]  
+        words_parts[word] = part
+    return words_parts
